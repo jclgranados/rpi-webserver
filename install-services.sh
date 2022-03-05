7d65407a8c2f80f0fb114155281be463652a5187
jclgranados <34133265+jclgranados@users.noreply.github.com>
2022-03-05 10:13:06 +0100

    Create install-services.sh

---
+++ install-services.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Script for install apache, mariadb, php and phpmyadmin for Raspberry pi OS
+# Javier Lacave jclgranados@gmail.com
+
+echo "Instalamos apache"
+echo "-----"
+apt install -y apache2
+echo "-----"
+echo ""
+echo "Damos permisos y owner correctos"
+echo "-----"
+chown -R www-data:www-data /var/www/html
+find /var/www/html -type d -print -exec chmod 775 {} \;
+find /var/www/html -type f -print -exec chmod 664 {} \;
+usermod -a -G www-data nube
+echo "-----"
+echo ""
+echo ""
+echo "Instalamos mariadb"
+echo "-----"
+apt install -y mariadb-server
+echo "-----"
+echo ""
+echo "Securizamos instalación, tendrás que responder algunas preguntas"
+echo "-----"
+mysql_secure_installation
+echo "-----"
+echo ""
+echo ""
+echo "Instalamos php"
+echo "-----"
+apt install php libapache2-mod-php php-mysql php-curl php-zip php-gd
+echo "-----"
+echo ""
+echo "Reiniciamos apache"
+echo "-----"
+systemctl restart apache2
+echo '<?php phpinfo(); ?>' > /var/www/html/info.php
+chown www-data. /var/www/html/info.php
+echo ""
+echo ""
+echo "Instalamos phpmyadmin"
+echo "-----"
+apt install -y phpmyadmin
+echo "-----"
